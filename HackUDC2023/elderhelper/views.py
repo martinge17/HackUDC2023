190ccc4ad84a3323f45e56a7eeb602e96718da47
miguimartinezzz <miguel.mmontans@udc.es>
2023-02-25 16:13:50 +0100

    jsdjaskjlasjdflasjlaskjdklasjdsa

--- HackUDC2023/elderhelper/views.py
+++ HackUDC2023/elderhelper/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 model_engine = "text-davinci-003"
 
 
@@ -23,7 +23,7 @@ def generate_text(request):
             max_tokens=1024,
             n=1,
             stop=None,
-            temperature=0.5,
+            temperature=0.57,
         )
         generated_text = completion.choices[0].text
         return render(request, 'output.html', {'generated_text': generated_text})
